3442cd0b209fd511b010bd2b063cf401cef625eb
felipemlucas13 <felipemlucas@poli.ufrj.br>
2026-06-08 19:54:02 -0300

    database.py

--- database.py
+++ database.py
@@ -76,7 +76,7 @@ def init_db():
                 finished INTEGER NOT NULL DEFAULT 0,
                 created_at TEXT NOT NULL,
                 FOREIGN KEY (phase_id) REFERENCES phases(id) ON DELETE CASCADE,
-                UNIQUE(phase_id, team_home, team_away, game_datetime),
+                UNIQUE(phase_id, team_home, team_away, game_datetime)
             );
 
             CREATE TABLE IF NOT EXISTS predictions (
